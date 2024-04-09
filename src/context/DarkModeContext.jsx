dad9a98e89f8c80c14d812a0698966ad9e922fdf
Soyi <frontsoyi@gmail.com>
2024-04-09 14:41:00 +0900

    modify darkmode

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext } from "react";
 import { useState } from "react";
 
 export const DarkModeContext = createContext();
